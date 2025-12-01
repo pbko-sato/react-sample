edb0638f3582920194abba4f1a14a5470fb8226b
pbko-sato <ko-sato@primebrains.co.jp>
2025-12-01 18:13:09 +0900

    2025/12/1 `Card`コンポーネントを切り出し

---
+++ src/components/atom/div/Card.tsx
@@ -0,0 +1,10 @@
+import { memo, type FC } from "react";
+import type { BaseComponentProps } from "types/styles/BaseComponentProps";
+
+interface CardProps extends BaseComponentProps {}
+
+export const Card: FC<CardProps> = memo(({ children, className, onClick }) => (
+  <div className={`card bg-base-100 w-full m-[5px] shadow-md${className && ` ${className}`}`} onClick={onClick}>
+    {children}
+  </div>
+));
